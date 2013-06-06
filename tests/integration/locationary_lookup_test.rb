85a3eb60221dc4cec4175defe745d18d5c705211
Oren Mazor <oren.mazor@gmail.com>
2013-06-06 12:09:16 -0400

    enable strict lookup
    Former-commit-id: 7d87e8d0af18d931bd68b5aef3c1bce9050af32d

--- tests/integration/locationary_lookup_test.rb
+++ tests/integration/locationary_lookup_test.rb
@@ -3,16 +3,21 @@ require "./lib/locationary"
 
 class LookupTests < MiniTest::Unit::TestCase
   def test_locationary_finds_by_longitude_latitude
-    actual = {:country => :Canada, :province => :Ontario, :city => :Ottawa, :zipcode => :K2K2K1}
-    assert_equal Locationary.find({:zipcode => :K2K2K1}), actual
-  end
-
-  def test_locationary_finds_by_zipcode
-    actual = {:country => :Canada, :province => :Ontario, :city => :Ottawa, :zipcode => :K2K2K1}
+    actual = {"Country Code"=>"CA", "Postal Code"=>"K2K", "Place Name"=>"Kanata (Beaverbrook / South March)", "Name 1"=>"Ontario", "Code 1"=>"ON", "Name 2"=>nil, "Code 2"=>nil, "Name 3"=>nil, "Code 3"=>nil, "Latitude"=>"45.3704", "Longitude"=>"-75.9198", "Accuracy"=>"1"}
     assert_equal Locationary.find({:longitude => actual[:longitude], :latitude => actual[:latitude]}), actual
   end
 
-  def test_locationary_accepts_passthrough
-    assert_equal Locationary.find({:postalcode => "foobar"}), nil
+  def test_locationary_finds_by_zipcode
+    actual = {"Country Code"=>"CA", "Postal Code"=>"K2K", "Place Name"=>"Kanata (Beaverbrook / South March)", "Name 1"=>"Ontario", "Code 1"=>"ON", "Name 2"=>nil, "Code 2"=>nil, "Name 3"=>nil, "Code 3"=>nil, "Latitude"=>"45.3704", "Longitude"=>"-75.9198", "Accuracy"=>"1"}
+    assert_equal Locationary.find({:postalcode => "K2K"}, {:strict => true}), actual
+  end
+
+  def test_strict_lookup_fails_quietly_on_wrong_data
+    assert_equal nil, Locationary.find({:postalcode => "foobar"},{:strict => true})
+  end
+
+  def test_strict_lookup_works_on_valid_data
+    actual = {"Country Code"=>"CA", "Postal Code"=>"K2K", "Place Name"=>"Kanata (Beaverbrook / South March)", "Name 1"=>"Ontario", "Code 1"=>"ON", "Name 2"=>nil, "Code 2"=>nil, "Name 3"=>nil, "Code 3"=>nil, "Latitude"=>"45.3704", "Longitude"=>"-75.9198", "Accuracy"=>"1"}
+    assert_equal actual, Locationary.find({:postalcode => "K2K"},{:strict => true})
   end
 end
