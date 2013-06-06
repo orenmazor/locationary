06e48ea3b46f1ae66e7f2112ffd36d46ea45ab06
Richard Blair <richard@blairingthoughts.com>
2013-06-06 11:58:42 -0400

    Add find_by methods
    Former-commit-id: bac97b3250013fa78e2e0140f29dccbf1c4e75b3

--- lib/locationary.rb
+++ lib/locationary.rb
@@ -19,4 +19,19 @@ module Locationary
     @data = MessagePack.unpack(Snappy.inflate(raw))
   end
 
+  PROPERTIES = {
+    postal_code: "Postal Code",
+    country_code: "Country Code",
+    state: "Name 1",
+    province: "Name 2",
+    community: "Name 3"
+  }
+
+  PROPERTIES.each do |location_prop|
+    class_eval <<-RUBY, __FILE__, __LINE__ +1
+      def Locationary.find_by_#{location_prop[0].to_s}(val)
+        Locationary.data
+      end
+    RUBY
+  end
 end
