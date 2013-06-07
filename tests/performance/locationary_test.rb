3500b2ea4398f742f829033a76e354f839e29266
Oren Mazor <oren.mazor@gmail.com>
2013-06-07 11:31:56 -0400

    add performance test for lookups

--- tests/performance/locationary_test.rb
+++ tests/performance/locationary_test.rb
@@ -8,7 +8,7 @@ class LocationaryPerformanceTests < MiniTest::Unit::TestCase
 
     raw = nil
     reading_speed = Benchmark.measure do
-      raw = File.read("#{Dir.pwd}/db/geonames.bin")
+      raw = File.read("#{Dir.pwd}/db/geonames_#{ENV['RACK_ENV']}.bin")
     end
     puts "reading: #{reading_speed}"
     assert reading_speed.real < 0.1
@@ -25,7 +25,21 @@ class LocationaryPerformanceTests < MiniTest::Unit::TestCase
       data = MessagePack.unpack(encoded)
     end
     puts "unpacking speed: #{unpacking_speed}"
-    assert unpacking_speed.real < 1
+    assert unpacking_speed.real < 2
 
+  end
+
+  def test_strict_lookup_speed
+    lookup_speed = Benchmark.measure do
+      Locationary.find("K2K2K1")
+    end
+
+    puts "initial lookup took #{lookup_speed.real} seconds"
+
+    lookup_speed = Benchmark.measure do
+      Locationary.find("K2K2K1")
+    end
+
+    puts "warmed up lookup took #{lookup_speed.real} seconds"
   end
 end
