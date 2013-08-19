1bffece4ac7fbf8b5e55e9543dfb3864e76e0620
Oren Mazor <oren.mazor@gmail.com>
2013-08-19 18:27:45 -0400

    fix db path

--- lib/locationary.rb
+++ lib/locationary.rb
@@ -45,7 +45,7 @@ module Locationary
   private
 
   def Locationary.load_data
-    raw = File.read("#{Dir.pwd}/db/geonames_#{ENV['RACK_ENV']}.bin")
+    raw = File.read("#{File.dirname(__FILE__)}/../db/geonames_#{ENV['RACK_ENV']}.bin")
     @data = MessagePack.unpack(Snappy.inflate(raw))
   end
 
